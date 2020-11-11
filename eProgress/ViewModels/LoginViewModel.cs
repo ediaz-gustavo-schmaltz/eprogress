4724eb3bf3c7049674dda709335fedee0b4b8112
Gustavo <gustavo.schmaltz@ediaz.com.br>
2020-11-11 15:36:22 -0300

    first commit

---
+++ eProgress/ViewModels/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using eProgress.Classes;
+using eProgress.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace eProgress.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private LoginWindow _loginWindow { get; set; }
+
+        private string login;
+        public string Login { get => login; set => SetProperty(ref login, value); }
+
+        public string Senha { get; set; }
+        public LoginViewModel(LoginWindow loginWindow)
+        {
+            _loginWindow = loginWindow;
+            MessageBoxCommand = new Command(() => {
+                MessageBox.Show("Eae");
+            });
+
+            MessageBoxCommand = new Command(() => {
+                MessageBox.Show("Eae");
+            });
+
+        }
+        public Command MessageBoxCommand { get; }
+
+        public Command CarregarGridCommand { get; }
+
+    }
+}
---
+++ eProgress/Views/LoginWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using eProgress.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace eProgress.View
+{
+    /// <summary>
+    /// Lógica interna para LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private LoginViewModel viewModel;
+        public LoginWindow()
+        {
+            InitializeComponent();
+            DataContext = viewModel = new LoginViewModel(this);
+        }
+    }
+}
